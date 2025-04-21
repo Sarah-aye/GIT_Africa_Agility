e501e8602561e9e1bea1542fb2c7e691c81280d7
Sarah-aye <sarahayen22@gmail.com>
2025-04-21 14:45:31 +0200

    another server-side error-fix

--- user_accounts/urls.py
+++ user_accounts/urls.py
@@ -13,9 +13,9 @@ router.register('farmerprofile_edit', views.FarmerProfileEditViewSet, basename='
 router.register('farmer_profile', views.FarmerProfileModelViewSet, basename='farmer_profile')
 
 urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('reset-password/', views.PasswordResetRequestView.as_view(), name='reset-password'),
+    path('api/register/', views.RegisterView.as_view(), name='register'),
+    path('api/login/', views.LoginView.as_view(), name='login'),
+    path('api/reset-password/', views.PasswordResetRequestView.as_view(), name='reset-password'),
 
     # Include all viewset routes
     path('', include(router.urls)),
